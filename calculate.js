1973b8ffd421149ac5bf9766d05a3391ab36868e
AH-sunny <sunnycse03@gmail.com>
2024-02-13 09:51:33 +0600

    add triangle calculation

--- calculate.js
+++ calculate.js
@@ -1,1 +1,20 @@
-const inputBase = document.getElementById('')
+
+
+function calculateTriangle(){
+    console.log('connected')
+    const inputBase = document.getElementById('tri_inp_base');
+    const baseTex = inputBase.value;
+    const base = parseFloat(baseTex);
+
+    const inputHeight = document.getElementById('tri_inp_height');
+    const triHeightTex = inputHeight.value;
+    const height =parseFloat(triHeightTex);
+
+    console.log(base);
+    console.log(height);
+
+    const areacal = (0.5 * base * height );
+    //display
+    const Sani = document.getElementById('tri-area');
+    Sani.innerText = areacal;
+}
